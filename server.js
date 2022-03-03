c36812b9ce41a1a5d372b3467b47d2cc604ffd89
Joshua Reyes <reyes.j15@gmail.com>
2022-03-03 17:26:09 -0500

    phase 1  complete

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/playlist_hw_db');
 
+
 const start = async() => {
     try{
         await sequelize.sync({force: true});
-
+        const red = Song.create('Red');
+        const views = Song.create('views');
+        const dawnFm = Song.create('Dawn FM');
 
     }
     catch(ex){
-        console.log(ex);
+        console.log(ex);    
     }
 };
 
