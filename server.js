3ccd55c29c87128e7ef1a12deaa54de0a346f04d
Joshua Reyes <reyes.j15@gmail.com>
2022-03-03 18:06:57 -0500

    fixed phase 1

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/playlist_hw_db');
-
+const Song = sequelize.define('song', {
+    name: {
+        type: Sequelize.STRING
+    }
+})
+const express = require('express');
+const app = express();
 
 const start = async() => {
     try{
@@ -8,6 +14,10 @@ const start = async() => {
         const red = Song.create('Red');
         const views = Song.create('views');
         const dawnFm = Song.create('Dawn FM');
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`));
+        
+        
 
     }
     catch(ex){
