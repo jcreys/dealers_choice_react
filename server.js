db947f3c92052b9b510aa6827985cdcad7f70aa6
Joshua Reyes <reyes.j15@gmail.com>
2022-03-03 21:15:45 -0500

    update phase 0 phase 1

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ const Song = sequelize.define('song', {
 })
 const express = require('express');
 const app = express();
-
+app.get('/api/songs',async (req,res,next) =>{
+    try{
+        res.send(await Song.findAll());
+    }
+    catch(ex){
+        next(ex);
+    }
+});
 const start = async() => {
     try{
         await sequelize.sync({force: true});
-        const red = Song.create('Red');
-        const views = Song.create('views');
-        const dawnFm = Song.create('Dawn FM');
+        await Promise.all([
+            Song.create({name: 'Red'}),
+            Song.create({name: 'Views'}),
+            Song.create({name: 'Out of Time'}),
+        ])
+        // const red = Song.create('Red');
+        // const views = Song.create('views');
+        // const dawnFm = Song.create('Dawn FM');
         const port = process.env.PORT || 3000;
         app.listen(port, () => console.log(`listening on port ${port}`));
         
